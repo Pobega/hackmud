652862348aaf3e3485506ab540df2d0d4fc9344f
Michael Pobega <pobega@gmail.com>
2018-03-20 04:51:56 -0400

    Figured out the nav page
    Changed it to a do...while loop to allow it to retry as long as
    there is corruption. It may be better to check both navigation
    words individually, but that costs more characters and I can only
    afford 500 right now :)

--- t1_scrape.js
+++ t1_scrape.js
@@ -1,10 +1,17 @@
 function(c, a) { // t:#s.username.target
-    let fp = a.t.call().split("\n"),   // Call front page and split by newline,
+  do {
+    var fp = a.t.call().split("\n"),   // Call front page and split by newline,
         n = fp[fp.length-1],           // just so we can get the last line.
+
         [d,e] = n.split(" | "),        // This splits our navigation pages into
                                        // the two separate page names we need.
-    re = /^[A-Za-z0-9_]+$/g,           // This regexp checks for corruption.
-    y = re.exec(n)                     // Run it against the nav page to avoid
-    if (!!y) return {ok:false, msg:n}  // having bad navigation.
-    return {ok:true, msg:n}            // This should only be hit if its good :)
+
+        re = /^[A-Za-z0-9_|\s]+$/g,    // This regexp checks for corruption.
+
+        y = re.test(n)                 // Run it against the nav page to avoid
+                                       // having corrupt navigation.
+
+  } while (!y)
+  return {ok:true, msg:n}  // This should only be hit if its good :)
+                           // Now we have our navigation.
 }
