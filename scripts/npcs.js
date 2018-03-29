10cc7278c6c3fc5df9775b35db97f2e562609db6
Michael Pobega <pobega@gmail.com>
2018-03-29 12:14:17 -0400

    Simplifying dicts and working on adding object:true support

--- scripts/npcs.js
+++ scripts/npcs.js
@@ -1,22 +1,28 @@
 function (c,a) { // sec:"all"
 
     // Security levels with their associated colors, to be output later.
-    let avail = {0: {level:"`eNULLSEC`", scripts:[]},
-                 1: {level:"`DLOWSEC`", scripts:[]},
-                 2: {level:"`5MIDSEC`", scripts:[]},
-                 3: {level:"`JHIGHSEC`", scripts:[]},
-                 4: {level:"`2FULLSEC`", scripts:[]}},
+    let avail = {fullsec: [],
+                 highsec: [],
+                 midsec:  [],
+                 lowsec:  [],
+                 nullsec: []},
 
-        map_args = {full: 4,
-                    fullsec: 4,
-                    high: 3,
-                    highsec: 3,
-                    midsec: 2,
-                    mid: 2,
-                    low: 1,
-                    lowsec: 1,
-                    "null": 0,
-                    "nullsec": 0},
+        map_level = {4: "fullsec",
+                     3: "highsec",
+                     2: "midsec",
+                     1: "lowsec",
+                     0: "nullsec"},
+
+        map_args = {full: "fullsec",
+                    fullsec: "fullsec",
+                    high: "highsec",
+                    highsec: "highsec",
+                    midsec: "midsec",
+                    mid: "midsec",
+                    low: "lowsec",
+                    lowsec: "lowsec",
+                    "null": "nullsec",
+                    "nullsec": "nullsec"},
 
     // All current NPC corps
         corps = ["amal_robo",
@@ -77,28 +83,30 @@ function (c,a) { // sec:"all"
                    "private",
                    "public"]
 
-    function get_scripts(level) {
-        let output = `${avail[level]["level"]} (${avail[level]["scripts"].length})\n`
-        avail[level]["scripts"].forEach((script) => {
-            output += `    ${script}\n`
-        })
-        return output
-    }
+//    function get_scripts(level) {
+//        let output = `${avail[level]["level"]} (${avail[level]["scripts"].length})\n`
+//        avail[level]["scripts"].forEach((script) => {
+//            output += `    ${script}\n`
+//        })
+//        return output
+//    }
 
     corps.forEach((corp) => { // For each corp
         scripts.forEach((script) => { // And for each script name
             let corp_script = `${corp}.${script}`,
-                level = #fs.scripts.get_level({name:corp_script})
+                level = map_level[#fs.scripts.get_level({name:corp_script})]
             // If the sec level is in the list of sec levels (1~4)
             if (!(avail[level] === undefined))
                 // Push it to the results.
-                avail[level]["scripts"].push(corp_script)
+                avail[level].push(corp_script)
         })
     })
 
-    // Not getting fancy here, just want to show the scripts in
-    // decreasing order of security level.
-    let output ="\n"
+    // todo: return avail or avail[level] if a.object is true
+    if (a.object)
+        return avail
+
+    let output="\n"
 
     try { // If you passed an arg to see just one sec level
         let level = map_args[a.sec]
