62bfc8e139d87b41987c2ff415d2ca353ac3c1e3
Michael Pobega <pobega@gmail.com>
2018-04-04 08:24:48 -0400

    Golf and comments
    Golfed the code a little bit (down to 224 chars!) and added comments.

--- scripts/decorrupt.js
+++ scripts/decorrupt.js
@@ -1,11 +1,20 @@
 function (c,a) {
-    let n = /(¡|¢|£|¤|¥|¦|§|¨|©|ª|Ã|Á)/, // Corruption char regex
-        s = /`\w(.)`/g, // Regex to strip colors
-        o = a.t.call().replace(s, "$1").split("") // Original script output
+        // Corruption char regex
+    let r = /(¡|¢|£|¤|¥|¦|§|¨|©|ª|Ã|Á)/,
+        // Regex to strip colors
+        s = /`\w(.)`/g,
+        // Original script output
+        d = function() { return a.t.call().replace(s, "$1").split("") },
+        // Original script output
+        o = d()
 
-    while (n.test(o.join(""))) {
-        a.t.call().replace(s, "$1").split("").forEach((p, i) => {
-            if (n.test(o[i]) && !(n.test(p)))
+    // continue until original output has no more corruption
+    while (r.test(o.join(""))) {
+        // iterate over a fresh output
+        d().forEach((p, i) => {
+            // if the origin output has corruption and the new doesn't,
+            // replace the corrpution with the proper char
+            if (r.test(o[i]) && !(r.test(p)))
                     o[i] = p
         })
     }
