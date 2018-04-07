0a5a4916fd9c78b3bf6b20b7f99a6a41f782e6e2
Michael Pobega <pobega@gmail.com>
2018-04-06 22:14:57 -0400

    Continued refactor of NPCs for object:true support

--- scripts/npcs.js
+++ scripts/npcs.js
@@ -1,18 +1,15 @@
 function (c,a) { // sec:"all"
 
-    // Security levels with their associated colors, to be output later.
-    let avail = {fullsec: [],
+    let lib = #fs.scripts.lib(),
+
+        // Security levels with their associated colors, to be output later.
+        avail = {fullsec: [],
                  highsec: [],
                  midsec:  [],
                  lowsec:  [],
                  nullsec: []},
 
-        map_level = {4: "fullsec",
-                     3: "highsec",
-                     2: "midsec",
-                     1: "lowsec",
-                     0: "nullsec"},
-
+        // Map sec: args to their respective keys in avail
         map_args = {full: "fullsec",
                     fullsec: "fullsec",
                     high: "highsec",
@@ -24,7 +21,7 @@ function (c,a) { // sec:"all"
                     "null": "nullsec",
                     "nullsec": "nullsec"},
 
-    // All current NPC corps
+        // All current NPC corps
         corps = ["amal_robo",
                  "aon",
                  "archaic",
@@ -83,18 +80,19 @@ function (c,a) { // sec:"all"
                    "private",
                    "public"]
 
-//    function get_scripts(level) {
-//        let output = `${avail[level]["level"]} (${avail[level]["scripts"].length})\n`
-//        avail[level]["scripts"].forEach((script) => {
-//            output += `    ${script}\n`
-//        })
-//        return output
-//    }
+    function get_scripts(level) {
+        let output = `${avail[level]["level"]} (${avail[level]["scripts"].length})\n`
+        avail[level]["scripts"].forEach((script) => {
+            output += `    ${script}\n`
+        })
+        return output
+    }
 
     corps.forEach((corp) => { // For each corp
         scripts.forEach((script) => { // And for each script name
             let corp_script = `${corp}.${script}`,
-                level = map_level[#fs.scripts.get_level({name:corp_script})]
+                level_int = #fs.scripts.get_level({name:corp_script}),
+                level = lib.get_security_level_name(level_int).toLowerCase()
             // If the sec level is in the list of sec levels (1~4)
             if (!(avail[level] === undefined))
                 // Push it to the results.
@@ -102,18 +100,29 @@ function (c,a) { // sec:"all"
         })
     })
 
-    // todo: return avail or avail[level] if a.object is true
-    if (a.object)
-        return avail
+    // return avail or avail[level] if a.object is true
+    if (a.object) {
+        if (a.sec) {
+            if (map_args[a.sec])
+                // https://stackoverflow.com/a/11508530
+                return {[map_args[a.sec]]: avail[map_args[a.sec]]}
+            else
+                return {ok:false, msg:"please use a valid sec level"}
+        }
+        else
+            return avail
+    }
 
     let output="\n"
 
+    // TODO: Get the output correctly using new data structures.
     try { // If you passed an arg to see just one sec level
         let level = map_args[a.sec]
         output += get_scripts(level)
     }
     catch(e) { // If not, show every sec level
-        output += "Use {sec:\"full\"} to see fullsec scripts. Default behavior is {sec:\"all\"}\n\n"
+        output += "Use {sec:\"full\"} to see fullsec scripts. Default behavior is {sec:\"all\"}\n"
+        output += "Use {object:true} to get JSON output for use in scripts.\n\n"
         for ( let i=4; i>=0; i-- )
             output += get_scripts(i) + "\n"
     }
