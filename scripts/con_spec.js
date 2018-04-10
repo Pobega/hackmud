b73015577253e73079ad977aba63c09332399df9
Michael Pobega <pobega@gmail.com>
2018-04-10 00:10:26 -0400

    Added USAGE pages to all scriptors/subscripts

--- scripts/con_spec.js
+++ scripts/con_spec.js
@@ -4,22 +4,22 @@ function (c,a) { // T2 CON_SPEC passes in {s:"string", d:digit}
 
     // Ensure that args were passed, and that those args are exactly
     // d:<digit> and s:<string>. Anything else, show the usage screen.
-    if (c.is_scriptor) {
+    if (c.is_scriptor || c.calling_script)
         return (args.s.match(new RegExp(args.d,"g")) || []).length
-    } else {
-        return {ok:false, msg:`
-Tier 2 ${lib.color("D", "CON_SPEC")} lock scriptor
+    else
+        return `
+:::pobega.con_spec:::
+
 
 ${lib.color("2", "DESCRIPTION")}
 
+  Tier 2 ${lib.color("D", "CON_SPEC")} lock scriptor.
+
   Counts how many occurences of the digit (\`Nd\`) are in the string (\`Ns\`).
+
 
 ${lib.color("2", "USAGE")}
 
   As a scriptor: \{ ${lib.color("N", "CON_SPEC")}:#s.${c.this_script}\ }
-
-  To test: ${c.this_script} \{ ${lib.color("N", "s")}:${lib.color("V", "<string>")}, ${lib.color("N", "d")}:${lib.color("V", "<int>")} \}
-`}
-    }
-
+`
 }
