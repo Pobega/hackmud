48e247eef94a6a2da3da80e8b5cffcc1aca05696
Michael Pobega <pobega@gmail.com>
2018-04-10 00:21:26 -0400

    Initial commit of balance script
    Tallies your balance from accts.balance, jade.vita and pay.pal

---
+++ scripts/balance.js
@@ -0,0 +1,17 @@
+function (c,a) {
+    let l = #fs.scripts.lib(),
+        bal = #hs.accts.balance(),
+        pay = /Your `2available` balance is ([\w]+)./g.exec(#fs.pay.pal())[1],
+        jade = /Account balance == ([\w]+GC)/g.exec(#fs.jade.vita())[1],
+        total = bal + l.to_gc_num(pay) + l.to_gc_num(jade)
+
+        return `
+:::pobega.balance:::
+
+accts.balance    ${l.to_gc_str(bal)}
+pay.pal          ${pay}
+jade.vita        ${jade}
+                 ---------
+\`1total\`            ${l.to_gc_str(total)}
+`
+}
